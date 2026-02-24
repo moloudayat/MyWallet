7efc44dfad61589a2582afaad5caf974c34a7b27
moloud <moloud.ayat@gmail.com>
2026-02-24 22:33:59 +0330

    [ADD] sign up icons

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -1,4 +1,10 @@
-const selfie = require("./images/selfie.png");
-const passport = require("./images/passport.png");
-
+// images
+const selfie = require('./images/selfie.png');
+const passport = require('./images/passport.png');
 export { selfie, passport };
+
+// icones
+export { default as UserIcon } from './icones/MingcuteUser3Fill';
+export { default as EmailIcon } from './icones/MingcuteMailFill';
+export { default as SunIcon } from './icones/MingcuteSunFill';
+export { default as MoonIcon } from './icones/MingcuteMoonStarsFill';
